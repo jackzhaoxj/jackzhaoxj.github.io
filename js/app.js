6f4b20a53d87d5fb2cf2e611a0ea42c0d8c328ce
jackzhaoxj <35189777+jackzhaoxj@users.noreply.github.com>
2018-12-21 01:56:07 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,5 +37,23 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000); //Days, Hours, Minutes, Seconds 
+// 获取当前时间时间戳
+var currentTime = Date.now();
+ 
+// 获取指定时间时间戳
+// 注1：这里写的是2012年9月1日0时0分0秒
+// 注2：Javascript中月份是实际数字减1
+var targetTime = (new Date(2019, 6, 1, 0, 0, 0)).getTime();
+ 
+// 获取差值，如果指定日期早于现在，则为负数
+var offsetTime = targetTime-currentTime ;
+ 
+// 求绝对值，获取相差的时间
+//offsetTime = Math.abs(offsetTime);
+ 
+// 将时间转位天数
+// 注：Javascript中时间戳的单位是毫秒
+//var offsetDays = Math.floor(offsetTime / (3600 * 24 * 1e3));
+
+var deadline = new Date(Date.parse(new Date())+ offsetTime); //Days, Hours, Minutes, Seconds 
 initializeClock('clockdiv', deadline);
